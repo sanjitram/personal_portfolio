2ba3bc737b456b466ed56a928c5e6d671d6996eb
sanjitram <24je0666@iitism.ac.in>
2024-11-02 21:35:42 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -12,21 +12,20 @@ const Home = () => {
       strings: ["sanjitRam", "a webDeveloper", "available"],
       typeSpeed: 100,  
       backSpeed: 30,    
-      backDelay: 3000, 
+      backDelay: 3000,  
       loop: true,       
     };
 
     
     typedRef.current = new Typed('.typing-element', options);
 
-    
     return () => {
       typedRef.current.destroy();
     };
   }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', justifyContent: 'center' }}>
+    <div className="homee" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', justifyContent: 'center' }}>
       <img src={meimg} className="mee" alt="face" />
       <div>
         <h1>I'm</h1>
