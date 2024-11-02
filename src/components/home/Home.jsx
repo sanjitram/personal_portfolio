fd8d5f88620b3fd924c4f0997a935201d1df12ca
sanjitram <24je0666@iitism.ac.in>
2024-11-02 14:36:25 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,22 +4,22 @@ import './Home.css'
 import meimg from './me.jpg'
 
 const Home = () => {
-  const typedRef = useRef(null); // Create a reference for the Typed instance
+  const typedRef = useRef(null); 
 
   useEffect(() => {
-    // Options for Typed.js
+  
     const options = {
       strings: ["sanjitRam", "a webDeveloper", "available"],
-      typeSpeed: 100,  // Speed of typing in milliseconds
-      backSpeed: 30,    // Speed of backspacing in milliseconds
-      backDelay: 3000,  // Delay before starting to backspace
-      loop: true,       // Enable looping through the strings
+      typeSpeed: 100,  
+      backSpeed: 30,    
+      backDelay: 3000, 
+      loop: true,       
     };
 
-    // Initialize Typed.js on the ref element
+    
     typedRef.current = new Typed('.typing-element', options);
 
-    // Cleanup function to destroy Typed instance on unmount
+    
     return () => {
       typedRef.current.destroy();
     };
@@ -31,7 +31,7 @@ const Home = () => {
       <div>
         <h1>I'm</h1>
       </div>
-      <h1 className="typing-element"></h1> {/* This is where Typed.js will inject the typing effect */}
+      <h1 className="typing-element"></h1> {}
       <p>Welcome to my personal portfolio website. Check it out and get in touch!</p>
     </div>
   );
