483f78469a5de986d89612b83594166913f0b59d
sanjitram <24je0666@iitism.ac.in>
2024-11-03 16:41:50 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
       <h1 className="typing-element"></h1> {}
       <p>Welcome to my personal portfolio website. Check it out and get in touch!</p>
       <p>Features an Email contact form, Dark mode, Hosting on Vercel, and more...</p>
-      <a href='https://personal-portfolio-six-mu-24.vercel.app' target="_blank">VISIT PROJECT</a>
+      <a href='https://personal-portfolio-six-mu-24.vercel.app' target="_blank">VISIT WEBSITE</a>
     </div>
   );
 };
