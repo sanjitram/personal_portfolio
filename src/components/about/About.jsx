1a781f49fc9ecad41cf138855feda8cfc0a6cb3e
sanjitram <24je0666@iitism.ac.in>
2024-11-04 19:27:57 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaHtml5, FaCss3Alt, FaReact } from 'react-icons/fa';
+import { SiC, SiCplusplus, SiPython } from 'react-icons/si';
 import './About.css';
 
 const About = () => {
@@ -16,24 +18,22 @@ const About = () => {
               In this project I have worked with HTML, CSS and Javascript. I’m constantly learning new things to stay updated with the latest advancements in technology.
             </p>
             <p>
-                This particular webpage uses ReactJS to make it responsive. I used the library EmailJS to enable the client to contact me through email.
+              This particular webpage uses ReactJS to make it responsive. I used the library EmailJS to enable the client to contact me through email.
             </p>
           </div>
 
           <div className="skills-list">
-            <h3> Tech Stack</h3>
+            <h3>Tech Stack</h3>
             <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>ReactJS</li>
-              <li>C</li>
-              <li>C++</li>
-              <li>Python</li>
+              <li><FaHtml5 className="icon-html" /> HTML</li>
+              <li><FaCss3Alt className="icon-css" /> CSS</li>
+              <li><FaReact className="icon-react" /> ReactJS</li>
+              <li><SiC className="icon-c" /> C</li>
+              <li><SiCplusplus className="icon-cplusplus" /> C++</li>
+              <li><SiPython className="icon-python" /> Python</li>
             </ul>
           </div>
         </div>
-
-      
       </div>
     </section>
   );
