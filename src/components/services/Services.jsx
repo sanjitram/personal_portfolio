4f0c3da8dcc6c22b42a549b7fbe8b4f894cdd406
sanjitram <24je0666@iitism.ac.in>
2024-11-04 18:29:01 +0530

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,34 +5,62 @@ const Services = () => {
   return (
     <section id="services">
       <div className="services-container">
-        <h2>Skills</h2>
+        <h1>Skills</h1>
         
         <div className="services-grid">
           <div className="service">
-            <span className="service-icon">💻</span>
-            <h3>Functional Webpages</h3>
-            <p>
-              Building responsive and user-friendly websites using modern technologies like React, HTML, and CSS for seamless performance and amazing UI/UX.
-            </p>
+            <div className="service-card">
+              <div className="service-card-inner">
+                <div className="service-card-front">
+                  <span className="service-icon">💻</span>
+                  <h1>Functional Webpages</h1>
+                  
+                </div>
+                <div className="service-card-back">
+          
+                <p>
+                    Building responsive and user-friendly websites using modern technologies like React, HTML, and CSS for seamless performance and amazing UI/UX.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           
           <div className="service">
-            <span className="service-icon">📱</span>
-            <h3>Responsible Designs</h3>
-            <p>
-              Delivers a seamless user experience across devices without loss of functionality.
-            </p>
+            <div className="service-card">
+              <div className="service-card-inner">
+                <div className="service-card-front">
+                  <span className="service-icon">📱</span>
+                  <h1>Responsive Designs</h1>
+                  
+                </div>
+                <div className="service-card-back">
+            
+                <p>
+                    Delivers a seamless user experience across devices without loss of functionality.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           
           <div className="service">
-            <span className="service-icon">🔧</span>
-            <h3>Backend Development</h3>
-            <p>
-              Designing and building scalable server-side applications, APIs, and databases to manage backend services and ensure data integrity.
-            </p>
-            <p>
-            (Coming Soon)
-            </p>
+            <div className="service-card">
+              <div className="service-card-inner">
+                <div className="service-card-front">
+                  <span className="service-icon">🔧</span>
+                  <h1>Backend Development</h1>
+                  
+                  <h5>(Coming Soon)</h5>
+                </div>
+                <div className="service-card-back">
+                
+                <p>
+                    Designing and building scalable server-side applications, APIs, and databases to manage backend services and ensure data integrity.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
